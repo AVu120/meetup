083bc9ba8ed8e2ae307e5453ca15b70fb84a665d
Anthony Hien Vu <anthony.vu@team.telstra.com>
2021-05-10 08:12:17 +1000

    Add comments.

--- client/src/utils/formValidation.ts
+++ client/src/utils/formValidation.ts
@@ -1,12 +1,14 @@
-// Form properties
+// Common form properties
 export const NAME = "name";
 export const EMAIL = "email";
 export const PASSWORD = "password";
 export const CONFIRMED_PASSWORD = "confirmedPassword";
 
+// Email validation logic
 const emailRegex: RegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 export const isEmailValid = (email: string): boolean => emailRegex.test(email);
 
+// Password validation logic
 type TComparePasswordProps = { password: string; confirmedPassword: string };
 type TisConfirmedPasswordSameAsPassword = (
   passwords: TComparePasswordProps
